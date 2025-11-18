55977bfa9cc7f46d34a681803d8804dfe15c42d6
HugoResina <148769159+HugoResina@users.noreply.github.com>
2025-11-18 12:45:14 +0100

    Add files via upload

--- Collserola/Collserola.js
+++ Collserola/Collserola.js
@@ -2,21 +2,31 @@ let img;
 let x;
 let y;
 let scale;
+let slider;
+let meters;
 
 function preload(){
-    img = loadImage('Fotos/FotoMapa.png');
+  
+    inputed = false;
+     img = loadImage('Fotos/FotoMapa.png');
     scale = 1920 / 13.8;
    
+    slider = createSlider(0, 750)
+  slider.position(250, 20)
+  slider.size(750);
+  slider.input(drawCircle);
 }
 
 
 
 
 function setup() {
-  createCanvas(1920,1080);
+  createCanvas(1920,1080)
+ 
 
 
   image(img,0,0);
+  text(meters + ' metres', 250, 50)
  
 
 }
@@ -31,28 +41,32 @@ function keyPressed(){
  else if(key == 's'){
      SaveImg()
  }
+ else if(key == 'f'){
+   create()
+ }
 }
 
-function mousePressed(){
+function create(){
   //setup();
   x = mouseX;
   y = mouseY;
   
-  console.log( x  + '-')
-  
+  drawCircle()
+}
+function drawCircle(){
+    setup();
+  meters = slider.value() + 250;
   stroke(0)
   noFill();
   
   circle(x,y,5)
   
-  for(var i = 1; i < 15; i++){
-    circle(x,y, i*scale*2)
+  for(var i = 1; i < 15/(meters/1000) ; i++){
+    circle(x,y, i*scale*2*(meters/1000))
   }
-  
-  
 }
 
-
 function draw() {
+
 
 }
